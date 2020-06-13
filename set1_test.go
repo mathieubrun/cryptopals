95cfa6af318346f7211fd02439fb3cc94f0bb951
Mathieu BRUN <>
2020-06-13 22:09:54 +0200

    feat: set 1, challenge 5

--- set1_test.go
+++ set1_test.go
@@ -66,4 +66,17 @@ func Test_Set1(t *testing.T) {
 		assert.Equal(t, expectedKey, result.Key)
 		assert.Equal(t, expected, string(result.Plain))
 	})
+
+	t.Run("Challenge 5 : Implement repeating-key XOR", func(t *testing.T) {
+		// given
+		input := []byte("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
+		key := []byte("ICE")
+		expected, _ := hex.DecodeString("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
+
+		// when
+		result := algos.Xor(input, key)
+
+		// then
+		assert.Equal(t, expected, result)
+	})
 }
